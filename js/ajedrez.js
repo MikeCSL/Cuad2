10cef52b753b3a523e3be0695288ec887ef304f0
Miguel <maontiverosr@gmail.com>
2019-10-16 22:02:31 -0500

    version final

---
+++ js/ajedrez.js
@@ -0,0 +1,25 @@
+function esquinas(){$("#11, #18, #81, #88").css({"background": "#F8AA14"})}
+
+function superior(){$(".fila_1").css({"background":"#F8AA14"})}
+
+function inferior(){$(".fila_8").css({"background":"#F8AA14"})}
+
+function zeta (){$(".fila_8, .fila_1, .diag_norm").css({"background":"#F8AA14"})}
+
+function cruz (){$("div:nth-child(4)").css({"background":"#F8AA14"})}
+
+function slash_normal (){$(".diag_norm").css({"background":"#F8AA14"})}
+
+function triangulo (){$(".lat_izq, .fila_1, .diag_norm").css({"background":"#F8AA14"})}
+
+function equis (){$(".diag_norm, .diag_inv").css({"background" : "#F8AA14"})}
+
+function slash_invertido (){$(".diag_inv").css({"background": "#F8AA14"})}
+
+function lateral_izq (){$(".lat_izq, #11, #81").css({"background":"#F8AA14"})}
+
+function lateral_der (){$(".lat_der, #18, #88").css({"background":"#F8AA14"})}
+
+function cuadro_x (){$(".lat_der, .lat_izq, .fila_1, .fila_8, .diag_norm, .diag_inv").css({"background":"#F8AA14"})}
+
+function marco (){$(".lat_der, .lat_izq, .fila_1, .fila_8").css({"background":"#F8AA14"})}
